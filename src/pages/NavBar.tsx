af82c1df62b9059e6601c2ce10826661079edd21
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-02 12:42:05 +0200

    added an ul

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
               <div className="container flex h-16 items-center">
                 <div className="mr-4 hidden md:flex">
-
+                <ul>
                     <li>
                         <Link to="/" className="mr-6 flex items-center space-x-2">
                             <AlertTriangle className="h-6 w-6 text-rose-600" />
@@ -32,6 +32,7 @@ export default function NavBar() {
                         </Link>
                     </li>
                   </nav>
+                </ul>
                 </div>
                 <div className="flex flex-1 items-center justify-end space-x-4">
                   <Button variant="outline">
