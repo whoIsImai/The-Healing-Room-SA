1cb62005f691544d1af35dff7f5e2a9601bcb025
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-02 12:48:12 +0200

    removed white space

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,10 +1,12 @@
 import {Link } from 'react-router-dom'
 import { Button } from "@/components/ui/button"
 import { MoveLeft } from "lucide-react"
+import NavBar from './NavBar'
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[100dvh] px-4 text-center">
+      <NavBar/>
       <div className="space-y-6 max-w-md">
         <h1 className="text-9xl font-extrabold tracking-tight text-primary">404</h1>
         <div className="space-y-2">
