777ce7c6776254000373317b3a29b3e2957200b7
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-07 18:05:59 +0200

    wrote code for firebase

---
+++ src/utils/firebase.tsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: import.meta.env.apiKey ,
+    authDomain: import.meta.env.authDomain ,
+    projectId: import.meta.env.projectId ,
+    storageBucket: import.meta.env.storageBucket ,
+    messagingSenderId: import.meta.env.messagingSenderId ,
+    appId: import.meta.env.appId ,
+    measurementId: import.meta.env.measurementId 
+  }
+  
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig)
