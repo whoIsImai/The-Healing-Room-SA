6ebca018d74daf0c251fdc303e73b841d54ddcd6
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-12 17:24:49 +0200

    finishhed the code for getting certain user story

--- src/pages/Your-Story.tsx
+++ src/pages/Your-Story.tsx
@@ -1,12 +1,57 @@
-import type { StorySubmission } from "../logic/submit-story"
+import { useEffect, useState } from "react"
+import { StorySubmission } from "@/logic/submit-story"
+import YourStoryList from "@/logic/your-story-list"
+import NavBar from "@/components/ui/NavBar"
+import { collection, getDocs, getFirestore } from "firebase/firestore"
+import { app } from "@/utils/firebase"
+import { getAuth } from "firebase/auth"
 
+  const auth = getAuth(app)
 
-export function yourStories({ stories }: { stories: StorySubmission[] }){
-    if (stories.length === 0) {
-        return (
-          <div className="text-center py-12">
-            <p className="text-gray-500">You have no stories have been shared.</p>
+export function YourStories(){
+    const [stories, setStories] = useState<StorySubmission[]>([])
+
+    const user = auth.currentUser
+    useEffect(()=> {
+        if(user && user.email){
+        const fetchStories = async(email : string)=> {
+            const db = getFirestore(app)
+            const snapshot = await getDocs(collection(db, 'stories', email))
+
+                const data: StorySubmission[] = snapshot.docs.flatMap(doc => {
+                    const docData = doc.data()
+                    console.log("Doc data:", docData)
+                    return (docData.stories ?? []) as StorySubmission[];
+                  })
+                  setStories(data);
+              } 
+
+        fetchStories(user.email)
+    }
+    })
+
+    return (
+        <>
+            <NavBar />
+        <div className="container py-10 items-center sm:py-20">
+          <div className="space-y-8 flex flex-col items-center justify-center">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                Stories of Survival & Resilience
+              </h1>
+              <p className="text-gray-500 md:text-xl">
+                Real experiences shared by survivors to inspire hope and raise awareness.
+              </p>
+            </div>
+    
+            <div className="grid gap-8 lg:grid-cols-3">
+              <div className="lg:col-span-2">
+                <YourStoryList stories={stories} />
+              </div>
+            </div>
           </div>
-        )
-      }
+        </div>
+        </>
+      )
+
 }
