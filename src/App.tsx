4d6b393cd4bda13cebf4068ff8493a024e77b763
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-12 17:26:34 +0200

    added your stories function to a path

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Stories from './pages/Story'
 import NotFound from './pages/not-found'
 import TermsAndConditions from './pages/Ts&Cs'
 import Profile from './pages/Profile'
+import YourStories from './pages/Your-Story'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/Stories" element={<Stories />} />
         <Route path="/Terms" element={<TermsAndConditions />} />
         <Route path="/Profile" element={<Profile />} />
+        <Route path="/your-Stories" element={<YourStories/>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
